b6070fe693e78988b77b756e29fdba2bc88df035
Kauesamartino <kaue_samartino@hotmail.com>
2025-06-17 08:37:14 -0300

    Enhance post retrieval by including author information in the query. Added additional error logging entries for post retrieval failures in combined.log and error.log.

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,11 @@ import db from "../../prisma/db"; // Importando o db corretamente
 
 async function getAllPosts(page) {
   try {
-    const posts = await db.post.findMany();
+    const posts = await db.post.findMany({
+      include: {
+        author: true
+      }
+    });
     return { data: posts, prev: null, next: null };
   } catch (error) {
     logger.error('Falha ao obter posts', { error });
