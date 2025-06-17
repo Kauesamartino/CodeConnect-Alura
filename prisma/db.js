467b7dd2f8446d29aa46daa88a6b1ea0326968b9
Kauesamartino <kaue_samartino@hotmail.com>
2025-06-17 07:56:40 -0300

    Update database connection string, enhance logging for post retrieval, and add Prisma dependencies. Introduced createdAt and updatedAt fields in Post model.

---
+++ prisma/db.js
@@ -0,0 +1,5 @@
+const { PrismaClient } = require('../src/generated/prisma')
+const db = new PrismaClient()
+// use `prisma` in your application to read and write data in your DB
+
+export default db;
