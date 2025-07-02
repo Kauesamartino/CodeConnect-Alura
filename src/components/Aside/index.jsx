4d801c0dc86c56ba7359b7b2aa44357bbf16125f
Kauesamartino <kaue_samartino@hotmail.com>
2025-07-02 01:51:10 -0300

    Refactor Prisma schema to add comments and likes fields in Post model, and introduce Comment model for enhanced post interactions. Update .gitignore to exclude combined.log. Modify Aside component to wrap logo in a Link for improved navigation.

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -1,11 +1,14 @@
 import styles from './aside.module.css'
 import Image from 'next/image'
 import logo from './Logo.png'
+import Link from 'next/link'
 
 export const Aside = () => {
     return (
         <div className={styles.aside}>
-            <Image src={logo} alt='Logo'/>
+            <Link href={"/"}>
+                <Image src={logo} alt='Logo' />
+            </Link>
         </div>
     )
 }
--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -3,32 +3,31 @@ import { Avatar } from "../Avatar"
 import styles from "./cardpost.module.css"
 import Link from "next/link"
 
-export const CardPost = ({ post, highlight }) =>{
-    return(
-        <Link href={`/posts/${post.slug}`} className={styles.link}>
-            <article className={styles.card} style={{ width: highlight ? 993 : 486}}>
-            <header className={styles.header}>
-                <figure style={{ height: highlight ? 300 : 133}}>
-                    <Image 
-                        src={post.cover}
-                        alt={`Capa do post de titulo: ${post.title}`}
-                        fill
+export const CardPost = ({ post, highlight }) => {
+    return (
+            <article className={styles.card} style={{ width: highlight ? 993 : 486 }}>
+                <header className={styles.header}>
+                    <figure style={{ height: highlight ? 300 : 133 }}>
+                        <Image
+                            src={post.cover}
+                            alt={`Capa do post de titulo: ${post.title}`}
+                            fill
+                        />
+                    </figure>
+                </header>
+
+                <section className={styles.body}>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                    <Link href={`/posts/${post.slug}`}>Ver detalhes</Link>
+                </section>
+
+                <footer className={styles.footer}>
+                    <Avatar
+                        imageSrc={post.author.avatar}
+                        name={post.author.username}
                     />
-                </figure>
-            </header>
-
-            <section className={styles.body}>
-                <h2>{post.title}</h2>
-                <p>{post.body}</p>
-            </section>
-
-            <footer className={styles.footer}>
-                <Avatar 
-                    imageSrc={post.author.avatar}
-                    name={post.author.username}
-                />
-            </footer>
-        </article>
-        </Link>
+                </footer>
+            </article>
     )
 }
