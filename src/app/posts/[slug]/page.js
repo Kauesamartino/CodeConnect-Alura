ef18f6910092348c7fdb893c21e3031dd9ddd953
Kauesamartino <kaue_samartino@hotmail.com>
2025-07-10 11:21:26 -0300

    Add postReply action to handle replies to comments and update post retrieval to include author information for comments. Integrate CommentList component in post page for displaying comments.

--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -6,38 +6,43 @@ import styles from './page.module.css'
 import { CardPost } from "@/components/CardPost";
 import db from "../../../../prisma/db";
 import { redirect } from "next/navigation";
+import { CommentList } from "@/components/CommentList";
 
 
 async function getPostBySlug(slug) {
 
     try {
         const post = await db.post.findFirst({
-            where: { 
+            where: {
                 slug
             },
             include: {
                 author: true,
-                comments: true
+                comments: {
+                    include: {
+                        author: true
+                    }
+                }
             }
         })
 
         if (!post) {
             throw new Error(`Post com o slug ${slug} não foi encontrado`)
         }
-    
+
         const processedContent = await remark()
             .use(html)
             .process(post.markdown);
         const contentHtml = processedContent.toString();
-    
+
         post.markdown = contentHtml
-    
+
         return post
     } catch (error) {
         logger.error('Falha ao obter o post com o slug: ', {
             slug,
             error
-        })   
+        })
     }
     redirect('/not-found')
 }
@@ -50,6 +55,7 @@ const PagePost = async ({ params }) => {
         <div className={styles.code}>
             <div dangerouslySetInnerHTML={{ __html: post.markdown }} />
         </div>
+        <CommentList comments={post.comments} />
     </div>)
 }
 
