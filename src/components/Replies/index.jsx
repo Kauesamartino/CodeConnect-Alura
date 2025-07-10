3997cceb9f59a1897d8e937ecdcbafe33d4cd37c
Kauesamartino <kaue_samartino@hotmail.com>
2025-07-10 12:32:22 -0300

    Enhance Replies component to fetch and display replies to comments, passing the comment data as a prop. Update CommentList to include Replies component with the necessary comment context. Add error log entries for failed post retrieval with specific slugs in combined.log and error.log for better debugging.

--- src/components/Replies/index.jsx
+++ src/components/Replies/index.jsx
@@ -1,21 +1,43 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styles from './replies.module.css'
+import { Comment } from '../Comment'
+import { ModalReply } from '../ModalReply'
 
-export const Replies = () => {
+export const Replies = ({ comment }) => {
 
     const [showReplies, setShowReplies] = useState(false)
+
+    const [replies, setReplies] = useState([])
+
+    async function fetchData() {
+        const reponse = await fetch(`/api/comment/${comment.id}/replies`)
+        const data = await reponse.json()
+        setReplies(data)
+    }
+
+    useEffect(() =>{
+        if (showReplies){
+            fetchData()
+        }
+    }, [showReplies])
 
     return (<div className={styles.container}>
 
         <div className={styles.replies}>
-            <button 
-                className={styles.btn} 
+            <button
+                className={styles.btn}
                 onClick={() => setShowReplies(!showReplies)}
             >
                 {showReplies ? 'Ocultar' : 'Ver'} respostas
             </button>
+            {showReplies && <ul>
+                {replies.map(reply => <li key={reply.id}>
+                    <Comment comment={reply}/>
+                    <ModalReply comment={reply}/>
+                </li>)}
+            </ul>}
         </div>
     </div>)
 }
